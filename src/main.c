f1781915e7ddc88831cd589a03e342591b1ba30f
oasdflkjo <petri.pihla@gmail.com>
2023-12-30 03:20:07 +0200

    real init :)

--- src/main.c
+++ src/main.c
@@ -1,10 +1,5 @@
-// make clean includes cos this hacking to avoid name collitions
-
-#define RAYLIB_CUSTOM_RAYLIB_H
+// workaround for naming conflicts, still prefer c tough :)
 #include "raylib.h"
-
-
-
 #define Rectangle _Rectangle
 #define CloseWindow _CloseWindow
 #define ShowCursor _ShowCursor
@@ -13,13 +8,12 @@
 #undef CloseWindow
 #undef ShowCursor
 
-#include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
 #include <xmmintrin.h>
+#include <assert.h>
 
-#define PARTICLE_COUNT 120000
-#define PARTICLE_COLOR (Color){0, 0, 0, 255}
+#define PARTICLE_COUNT 720000 // TODO: crashes if used for example 130000 
+#define PARTICLE_COLOR \
+    (Color) { 0, 0, 0, 255 }
 #define MAX_THREADS 12
 #define ALIGNMENT 16
 #define FRAME_HISTORY_COUNT 10
@@ -37,70 +31,48 @@ typedef struct Particles
     float *velY;
 } Particles;
 
-typedef struct {
+typedef struct
+{
     int start;
     int end;
     Particles *particles;
     Vector2 mousePos;
 } ThreadArgs;
 
-
 Particles CreateParticles(int count, int screenWidth, int screenHeight);
 void UpdateParticlesMultithreaded(Particles *particles, Vector2 mousePos);
-void DrawParticles(Particles *particles, int count);
 void FreeParticles(Particles *particles);
+void HandleKeys(void);
+void UpdateOffScreenBufferWithParticles(Particles *particles, Texture2D *offScreenBuffer);
 
-unsigned char drawnPixels[SCREEN_WIDTH * SCREEN_HEIGHT] = {0};
 static float globalAttractionStrength = ATTRACTION_STRENGHT;
 static float globalFriction = FRICTION;
 
 int main()
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Particle Effect in C");
+    assert(PARTICLE_COUNT % 4 == 0 && "Particle count must be a multiple of 4 for SIMD operations");
 
-    double frameTimes[FRAME_HISTORY_COUNT] = {0};
-    int frameCount = 0;
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Particle Effect in C");
+    Image particleImage = GenImageColor(SCREEN_WIDTH, SCREEN_HEIGHT, BLANK);
+    Texture2D offScreenBuffer = LoadTextureFromImage(particleImage);
+    UnloadImage(particleImage); // Unload the image once texture is created
+
     Particles particles = CreateParticles(PARTICLE_COUNT, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
     {
-
-        if (IsKeyPressed(KEY_UP))
-        {
-            globalAttractionStrength += 0.0005f;
-            TraceLog(LOG_INFO, "Attraction: %.4f, Friction: %.4f", globalAttractionStrength, globalFriction);
-        }
-        if (IsKeyPressed(KEY_DOWN))
-        {
-            globalAttractionStrength -= 0.0005f;
-                    TraceLog(LOG_INFO, "Attraction: %.4f, Friction: %.4f", globalAttractionStrength, globalFriction);
-        }
-        if (IsKeyPressed(KEY_RIGHT))
-        {
-            globalFriction += 0.0005f;
-                    TraceLog(LOG_INFO, "Attraction: %.4f, Friction: %.4f", globalAttractionStrength, globalFriction);
-        }
-        if (IsKeyPressed(KEY_LEFT))
-        {
-            globalFriction -= 0.0005f;
-                    TraceLog(LOG_INFO, "Attraction: %.4f, Friction: %.4f", globalAttractionStrength, globalFriction);
-        }
-        double startTime = GetTime();
+        HandleKeys();
 
         Vector2 mousePos = GetMousePosition();
         UpdateParticlesMultithreaded(&particles, mousePos);
-
-        double endTime = GetTime();
-        frameTimes[frameCount % FRAME_HISTORY_COUNT] = endTime - startTime;
-        frameCount++;
+        UpdateOffScreenBufferWithParticles(&particles, &offScreenBuffer);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawParticles(&particles, PARTICLE_COUNT);
+        DrawTexture(offScreenBuffer, 0, 0, WHITE);
         DrawFPS(10, 10);
-        char infoText[128];
         EndDrawing();
     }
 
@@ -128,12 +100,14 @@ Particles CreateParticles(int count, int screenWidth, int screenHeight)
     return p;
 }
 
-DWORD WINAPI UpdateParticlesSubset(LPVOID param) {
+DWORD WINAPI UpdateParticlesSubset(LPVOID param)
+{
     ThreadArgs *threadArgs = (ThreadArgs *)param;
     Particles *particles = threadArgs->particles;
     Vector2 mousePos = threadArgs->mousePos;
 
-    for (int i = threadArgs->start; i < threadArgs->end; i += 4) {
+    for (int i = threadArgs->start; i < threadArgs->end; i += 4)
+    {
         // Load positions and velocities of 4 particles into SIMD registers
         __m128 posX = _mm_load_ps(&particles->posX[i]);
         __m128 posY = _mm_load_ps(&particles->posY[i]);
@@ -178,59 +152,33 @@ DWORD WINAPI UpdateParticlesSubset(LPVOID param) {
     }
 }
 
-void UpdateParticlesMultithreaded(Particles *particles, Vector2 mousePos) {
+void UpdateParticlesMultithreaded(Particles *particles, Vector2 mousePos)
+{
     const int threadCount = MAX_THREADS;
     HANDLE threads[threadCount];
     ThreadArgs args[threadCount];
 
     int particlesPerThread = PARTICLE_COUNT / threadCount;
 
-    for (int i = 0; i < threadCount; i++) {
+    for (int i = 0; i < threadCount; i++)
+    {
         args[i].start = i * particlesPerThread;
         args[i].end = (i + 1) * particlesPerThread;
         args[i].particles = particles;
         args[i].mousePos = mousePos;
 
         threads[i] = CreateThread(NULL, 0, UpdateParticlesSubset, &args[i], 0, NULL);
-        if (threads[i] == NULL) {
+        if (threads[i] == NULL)
+        {
             // Handle thread creation error
         }
     }
 
-    for (int i = 0; i < threadCount; i++) {
+    for (int i = 0; i < threadCount; i++)
+    {
         WaitForSingleObject(threads[i], INFINITE);
         CloseHandle(threads[i]);
     }
-}
-
-// TODO: filter out particles outside the window
-void DrawParticles(Particles *particles, int count) {
-    for (int i = 0; i < count; i++) {
-        int x = (int)particles->posX[i];
-        int y = (int)particles->posY[i];
-
-        // Check bounds and if pixel is already drawn
-        if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT &&
-            !drawnPixels[y * SCREEN_WIDTH + x]) {
-            
-            DrawRectangle(x, y, 2, 2, PARTICLE_COLOR);
-
-            // Mark these pixels as drawn
-            drawnPixels[y * SCREEN_WIDTH + x] = 1;
-            if (x + 1 < SCREEN_WIDTH) {
-                drawnPixels[y * SCREEN_WIDTH + (x + 1)] = 1;
-            }
-            if (y + 1 < SCREEN_HEIGHT) {
-                drawnPixels[(y + 1) * SCREEN_WIDTH + x] = 1;
-                if (x + 1 < SCREEN_WIDTH) {
-                    drawnPixels[(y + 1) * SCREEN_WIDTH + (x + 1)] = 1;
-                }
-            }
-        }
-    }
-
-    // Reset drawnPixels for the next frame
-    memset(drawnPixels, 0, sizeof(drawnPixels));
 }
 
 void FreeParticles(Particles *particles)
@@ -239,4 +187,56 @@ void FreeParticles(Particles *particles)
     _aligned_free(particles->posY);
     _aligned_free(particles->velX);
     _aligned_free(particles->velY);
+}
+
+void UpdateOffScreenBufferWithParticles(Particles *particles, Texture2D *offScreenBuffer)
+{
+    Color *pixels = (Color *)malloc(SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Color));
+
+    // Initialize pixel data
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++)
+    {
+        pixels[i] = BLANK;
+    }
+
+    // Set particle pixels
+    for (int i = 0; i < PARTICLE_COUNT; i++)
+    {
+        int x = (int)particles->posX[i];
+        int y = (int)particles->posY[i];
+
+        if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
+        {
+            pixels[y * SCREEN_WIDTH + x] = PARTICLE_COLOR;
+        }
+    }
+
+    // Update the texture with new pixel data
+    UpdateTexture(*offScreenBuffer, pixels);
+
+    free(pixels);
+}
+
+void HandleKeys(void)
+{
+    if (IsKeyPressed(KEY_UP))
+    {
+        globalAttractionStrength += 0.0005f;
+        TraceLog(LOG_INFO, "Attraction: %.4f, Friction: %.4f", globalAttractionStrength, globalFriction);
+    }
+    if (IsKeyPressed(KEY_DOWN))
+    {
+        globalAttractionStrength -= 0.0005f;
+        TraceLog(LOG_INFO, "Attraction: %.4f, Friction: %.4f", globalAttractionStrength, globalFriction);
+    }
+    if (IsKeyPressed(KEY_RIGHT))
+    {
+        globalFriction += 0.0005f;
+        TraceLog(LOG_INFO, "Attraction: %.4f, Friction: %.4f", globalAttractionStrength, globalFriction);
+    }
+    if (IsKeyPressed(KEY_LEFT))
+    {
+        globalFriction -= 0.0005f;
+        TraceLog(LOG_INFO, "Attraction: %.4f, Friction: %.4f", globalAttractionStrength, globalFriction);
+    }
 }
